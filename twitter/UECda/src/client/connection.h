f23d90e9b9a28f0dd99de0474e9890347b39d294
Hitoshi Yoshida <github@peria.jp>
2017-08-29 22:11:45 +0900

    Convert file to .cc and format

--- twitter/UECda/src/client/connection.h
+++ twitter/UECda/src/client/connection.h
@@ -1,12 +1,11 @@
-/*connection*/ 
+#pragma once
 
-void checkArg(int argc,char* argv[]);
-int  startGame(int table[8][15]);
-int  entryToGame(void);
+void checkArg(int argc, char* argv[]);
+int startGame(int table[8][15]);
+int entryToGame(void);
 void sendChangingCards(int cards[8][15]);
-int  receiveCards(int cards[8][15]);
-int  sendCards(int cards[8][15]);
+int receiveCards(int cards[8][15]);
+int sendCards(int cards[8][15]);
 void lookField(int cards[8][15]);
-int  beGameEnd(void);
-int  closeSocket();
-
+int beGameEnd(void);
+int closeSocket();
