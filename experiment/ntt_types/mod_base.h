e5807fdb472639eaa506f509df1a0b620d77e570
Hitoshi Yoshida <github@peria.jp>
2022-01-26 01:14:41 +0900

    Make mod.h easy to replace

---
+++ experiment/ntt_types/mod_base.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "base.h"
+
+template <uint64 P>
+class Mod {
+ public:
+  Mod() = default;
+  explicit Mod(uint64 x) : x_(x) {}
+  Mod<P>& operator=(const Mod<P>& x) {
+    x_ = x;
+    return *this;
+  }
+  Mod<P>& operator=(uint64 x) {
+    x_ = x;
+    return *this;
+  }
+
+  operator uint64() const { return x_; }
+
+  Mod<P> operator+(const Mod<P>& y) { return operator-(Mod<P>(P - y.x_)); }
+  Mod<P>& operator+=(const Mod<P>& y) { return operator-=(Mod<P>(P - y.x_)); }
+
+  Mod<P> operator-(const Mod<P>& y) {
+    uint64 d = x_ - y.x_;
+    if (x_ < y.x_) {
+      d += P;
+    }
+    return Mod<P>(d);
+  }
+  Mod<P>& operator-=(const Mod<P>& y) {
+    uint64 d = x_ - y.x_;
+    if (x_ < y.x_) {
+      d += P;
+    }
+    x_ = d;
+    return *this;
+  }
+
+  Mod<P>& operator*=(const Mod<P>& y) {
+    uint128 prod = uint128(x_) * y.x_;
+    x_ = prod % P;
+    return *this;
+  }
+  Mod<P> operator*(const Mod<P>& y) {
+    uint128 prod = uint128(x_) * y.x_;
+    return Mod<P>(uint64(prod % P));
+  }
+
+  Mod<P>& operator>>=(int64 k) {
+    x_ >>= k;
+    return *this;
+  }
+
+ private:
+  uint64 x_;
+};
