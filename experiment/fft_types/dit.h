a3c210b7551e48cc87cff8d23c584157c9ec6bbb
Hitoshi Yoshida <github@peria.jp>
2023-03-04 16:01:56 +0900

    Impl IDFT4 for DIT

--- experiment/fft_types/dit.h
+++ experiment/fft_types/dit.h
@@ -97,7 +97,12 @@ void DIT::Dft(Complex* x) const {
 void DIT::IDft(Complex* x) const {
   int m = 1;
   int l = n_;
-  for (int i = 0; i < logn_; ++i) {
+  for (int i = 0; i < log4n_; ++i) {
+    l /= 4;
+    IDft4(x, m, l);
+    m *= 4;
+  }
+  for (int i = 0; i < log2n_; ++i) {
     l /= 2;
     IDft2(x, m, l);
     m *= 2;
@@ -159,6 +164,32 @@ void DIT::Dft4(Complex* x, const int m, const int l) const {
       x[k1] = y0 - y1;
       x[k2] = y2 + y3;
       x[k3] = y2 - y3;
+    }
+  }
+}
+
+void DIT::IDft4(Complex* x, const int m, const int l) const {
+  for (int j = 0; j < l; ++j) {
+    const Complex w1 = ws_[2 * j].conj();
+    const Complex w2 = ws_[j].conj();
+    const Complex w3 = w1 * w2;
+    for (int k = 0; k < m; ++k) {
+      int k0 = 4 * j * m + k;
+      int k1 = 4 * j * m + m + k;
+      int k2 = 4 * j * m + 2 * m + k;
+      int k3 = 4 * j * m + 3 * m + k;
+      const Complex& x0 = x[k0];
+      const Complex& x1 = x[k1];
+      const Complex& x2 = x[k2];
+      const Complex& x3 = x[k3];
+      const Complex y0 = x0 + x1;
+      const Complex y1 = x0 - x1;
+      const Complex y2 = x2 + x3;
+      const Complex y3 = (x3 - x2).i();
+      x[k0] = y0 + y2;
+      x[k1] = (y1 + y3) * w1;
+      x[k2] = (y0 - y2) * w2;
+      x[k3] = (y1 - y3) * w3;
     }
   }
 }
